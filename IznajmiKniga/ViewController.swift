f24b594a85621e405662226da8f3345e7e59c0a8
simona <simona@simonas-Mac.local>
2021-01-12 02:01:32 -0800

    Initial Commit

---
+++ IznajmiKniga/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IznajmiKniga
+//
+//  Created by simona on 1/12/21.
+//  Copyright © 2021 simona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
